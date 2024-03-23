d6d54af864acaf7f91a77d30787cb8e320b84284
sofia <sofialabellaja21@gmail.com>
2024-03-23 16:30:28 -0300

    carga_inicial

---
+++ primos.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Python program to display all the prime numbers within an interval
+
+# Los números van desde 1 hasta 100
+lower = 1
+upper = 100
+
+print("Prime numbers between", lower, "and", upper, "are:")
+#imprime los numeros primos entre 1 y 100
+
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num) 
