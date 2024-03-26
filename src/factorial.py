b3cedab743c075b4e06c045e242682db9d6a901f
sofia <sofialabellaja21@gmail.com>
2024-03-26 10:04:43 -0300

    rangos sin límite inferior

--- src/factorial.py
+++ src/factorial.py
@@ -22,10 +22,24 @@ def factorial(num):
 
 if len(sys.argv) < 2:
     range_input = input("Por favor, ingresa el rango (desde-hasta) para calcular los factoriales: ")
-    start, end = map(int, range_input.split("-"))
 else:
     range_input = sys.argv[1]
-    start, end = map(int, range_input.split("-"))
+    
+if "-" not in range_input:
+    print("El formato del rango es incorrecto. Debe ser 'desde-hasta'.")
+    sys.exit()
+
+start, end = map(int, range_input.split("-"))
+start = int(start) if start else 1
+end = int(end) if end else 60
+
+if start <= 0 or end <= 0:
+    print("Los números negativos no son válidos.")
+    sys.exit()
+
+if start > end:
+    print("El número de inicio debe ser menor o igual que el número final.")
+    sys.exit()
 
 for num in range(start, end+1):
     print("Factorial", num, "! es", factorial(num))
